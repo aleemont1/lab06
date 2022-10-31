6d815a22acf3a7c930a18fd9b4284b142d146592
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2022-10-31 18:58:04 +0100

    Improved 62.2

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -44,8 +44,7 @@ public final class UseListsAndMaps {
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
-        final var list2 = new LinkedList<Integer>();
-        list2.addAll(list);
+        final var list2 = new LinkedList<Integer>(list);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
